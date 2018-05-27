b8f9c030140025b9a6b649a7ec7c6b0f7ccb447d
Ralf S. Engelschall <rse@engelschall.com>
2018-05-27 21:11:11 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# microkernel-ext-cls
-Microkernel extension procedure for Continuation Local Storage (CLS)
+
+microkernel-ext-cls
+===================
+
+Microkernel extension procedure for Continuation Local Storage (CLS).
+
+<p/>
+<img src="https://nodei.co/npm/microkernel-ext-cls.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/microkernel-ext-cls.png" alt=""/>
+
+About
+-----
+
+This is an extension procedure for the
+[Microkernel](http://github.com/rse/microkernel) server
+application environment, adding a
+[Continuation Local Storage (CLS)](https://www.npmjs.com/package/continuation-local-storage) namespace.
+
+Usage
+-----
+
+```shell
+$ npm install microkernel microkernel-ext-cls
+```
+
+```js
+const Microkernel = require("microkernel")
+const kernel      = new Microkernel()
+
+kernel.exec("microkernel-ext-cls").then(() => {
+    kernel.load(...)
+    kernel.state("started").then(() => {
+        ...
+    }, (err) => {
+        ...
+    })
+})
+```
+
+License
+-------
+
+Copyright (c) 2018 Ralf S. Engelschall (http://engelschall.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ microkernel-ext-cls.js
@@ -0,0 +1,69 @@
+/*
+**  Microkernel -- Microkernel for Server Applications
+**  Copyright (c) 2018 Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*  external requirement  */
+const CLS = require("continuation-local-storage")
+
+/*  the Microkernel extension procedure  */
+const Extension = (kernel) => {
+    /*  create a continuation-local-storage (CLS) namespace  */
+    let ns = CLS.createNamespace("microkernel")
+    kernel.rs("cls", ns)
+
+    /*  provide wrapper services for lazy creation of new CLS contexts  */
+    kernel.register("cls-bind-fn", (fn) => {
+        return ns.bind(fn)
+    })
+    kernel.register("cls-bind-ee", (ee) => {
+        return ns.bindEmitter(ee)
+    })
+
+    /*  provide wrapper services for immediate creation of new CLS contexts  */
+    kernel.register("cls-run", (cb) => {
+        return ns.run((ctx) => cb(ctx))
+    })
+    kernel.register("cls-call", (cb) => {
+        return ns.runAndReturn((ctx) => cb(ctx))
+    })
+
+    /*  provide wrapper service for creation of CLS context  */
+    kernel.register("cls-create", () => {
+        return ns.createContext()
+    })
+
+    /*  provide wrapper services for accessing the currently active CLS context  */
+    kernel.register("cls-active", () => {
+        return ns.active
+    })
+    kernel.register("cls-get", (key) => {
+        return ns.get(key)
+    })
+    kernel.register("cls-set", (key, val) => {
+        return ns.set(key, val)
+    })
+}
+
+/*  export the Microkernel extension  */
+module.exports = Extension
+
